316b63ed818b42ad2dcc67ffe35fae5cd5f6aa16
ISimpForRaiynch <87456452+ISimpForRaiynch@users.noreply.github.com>
2021-07-15 19:42:04 -0500

    Add files via upload

---
+++ Temp Convertor C To F or F to C/Temp Convertor C To F or F to C/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Temp_Convertor_C_To_F_or_F_to_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ISFR Temp Convertor (C to F or F to C)");
+            Console.WriteLine("Please pick either C to F or F to C (Type 'C to F' or 'F to C)");
+            string myConver = Console.ReadLine();
+            Console.WriteLine("Awesome Now select a degree!");
+            double myDeg = double.Parse(Console.ReadLine());
+            if (myConver.Contains("C to F"))
+            {
+                double myNewDeg = ((myDeg * 9 / 5) + 32);
+                Console.WriteLine(myNewDeg);
+            }
+            else if (myConver.Contains("F to C"))
+            {
+                double mySecondDeg = ((myDeg - 32) * 5/9);
+                Console.WriteLine(mySecondDeg);
+            }
+        }
+    }
+}
